6418d1d118d201d0023b4ff22e2ef18ed5643d6a
Drakon <Drakon@DrakonPC>
2016-09-15 22:11:55 +0200

    example enhenced.

--- AppWindow/__example.py
+++ AppWindow/__example.py
@@ -7,12 +7,23 @@ Created on 15 wrz 2016
 import sys
 from PyQt4 import QtGui
 from AppWindow.CQAppWindow import CQAppWindow
+from AppWindow.CQAppPageWidget import CQAppPageWidget
+
+
+class PageWgtHome(CQAppPageWidget):    
+    
+    def __init__(self, appWindow):
+        super(CQAppPageWidget,self).__init__(appWindow);
+        appWindow.msgPrint('Hallo World!');
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
     
     win = CQAppWindow();
     win.show();
+    
+    pageHome = PageWgtHome(win);
     
     sys.exit(app.exec_())
     
